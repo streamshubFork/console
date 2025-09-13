1cfd64fc988328bbe83f551be7f5df9459ff8ff1
jkalinic <jkalinic@redhat.com>
2025-09-13 02:06:10 +0200

    [WIP] testGHA
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- .github/actions/systemtests/common.sh
+++ .github/actions/systemtests/common.sh
@@ -4,7 +4,6 @@ set -xeuo pipefail
 # Returns empty edit mode if the last bot comment is NOT the help message
 getEditModeIfLastCommentIsHelp() {
   local marker="ℹ️ Systemtests Help ℹ️"
-  local edit_existing=${1:-false}
   local comment_author=$(gh api user --jq '.login')
 
   local last_body=$(gh pr view "$PR_NUMBER" --repo "$REPO" \
@@ -15,7 +14,7 @@ getEditModeIfLastCommentIsHelp() {
           | last
           | .body // empty')
 
-  if [[ -n $last_body && $edit_existing == "true" && $last_body == *"$marker"* ]]; then
+  if [[ -n $last_body && $last_body != *"$marker"* ]]; then
     echo "--edit-last --create-if-none"
   else
     echo ""
